7740e4c31c08c818872718a6994086a368b15f00
rvosa <rutgeraldo@gmail.com>
2015-11-04 17:52:25 +0100

    adding driver script

---
+++ runall.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SPREADSHEET=data/taxa/sampling_asterids_tree.tsv
+SUPERSMART=data/supersmart
+
+# make top level working directory
+if [ ! -d "$SUPERSMART" ]; then
+	mkdir $SUPERSMART
+fi
+
+# make sub directories
+perl script/make_taxon_subsets.pl -i $SPREADSHEET -o $SUPERSMART
+
+# make list of taxon names
+TAXA=`ls $SUPERSMART/`
+
+# iterate over taxa
+for TAXON in $TAXA; do
+
+	# move into working directory
+	cd $SUPERSMART/$TAXON
+
+	# run smrt taxize
+	if [ ! -e "species.tsv" ]; then
+		smrt taxize -b -r $TAXON
+	fi
+	
+	# move back
+	cd -
+
+done
