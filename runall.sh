b85cb1b3ef5a47eca4060f98a605c73ab043f837
rvosa <rutgeraldo@gmail.com>
2015-11-06 11:35:01 +0100

    deleting initial run

--- runall.sh
+++ runall.sh
@@ -2,7 +2,9 @@
 
 SPREADSHEET=data/taxa/sampling_asterids_tree.tsv
 SUPERSMART=data/supersmart-merged
+MARKERS=data/markers
 export SUPERSMART_BACKBONE_MAX_DISTANCE=0.25
+export SUPERSMART_BACKBONE_MIN_COVERAGE=1
 
 # make top level working directory
 if [ ! -d "$SUPERSMART" ]; then
@@ -15,7 +17,7 @@ perl script/make_taxon_subsets.pl -i $SPREADSHEET -o $SUPERSMART
 # make list of taxon names
 TAXA=`ls $SUPERSMART/`
 
-# iterate over taxa
+# iterate over taxa to create marker tables
 for TAXON in $TAXA; do
 
 	# move into working directory
@@ -35,16 +37,29 @@ for TAXON in $TAXA; do
 	if [ -e "aligned.txt" ] && [ ! -e "merged.txt" ]; then
 		smrt orthologize
 	else
-		echo "no alignments were made for $TAXON, won't cluster"
+		if [ -e "merged.txt" ]; then
+			echo "already clustered $TAXON"
+		else
+			echo "no alignments were made for $TAXON, won't cluster"
+		fi
 	fi	
 	
 	# perform a supermatrix merger, produce marker table
 	if [ -e "merged.txt" ] && [ ! -e "markers-backbone.tsv" ]; then
 		smrt bbmerge --exemplars -1
 	else
-		echo "no clustering was done for $TAXON, won't merge"
+		if [ -e "markers-backbone.tsv" ]; then
+			echo "already merged $TAXON"
+		else
+			echo "no clustering was done for $TAXON, won't merge"
+		fi
 	fi	
 	
 	# move back
 	cd -
 done
+
+# create marker summary
+if [ ! -e "$MARKERS/markers.tsv" ]; then
+	perl script/count_markers.pl -i $SUPERSMART > "$MARKERS/markers.tsv"
+fi
