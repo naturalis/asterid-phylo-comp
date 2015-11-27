7b41616a2e0cc73c834076ed380dc329e0c70f33
rvosa <rutgeraldo@gmail.com>
2015-11-27 16:46:41 +0100

    supermatrix now has NCBI taxon IDs as row labels

--- script/merge_aln.pl
+++ script/merge_aln.pl
@@ -2,17 +2,24 @@
 use strict;
 use warnings;
 use Getopt::Long;
+use Bio::DB::GenBank;
 use Bio::Phylo::Factory;
+use Bio::Phylo::Util::Logger ':levels';
 use Bio::Phylo::IO qw'parse_matrix unparse';
 
 # process command line arguments
-my ( @infile, $format );
+my $format = 'phylip';
+my $verbosity = WARN;
+my @infile;
 GetOptions(
 	'infile=s' => \@infile,
 	'format=s' => \$format,
+	'verbose+' => \$verbosity,
 );
 
 # instantiate helper objects
+my $log = Bio::Phylo::Util::Logger->new( '-level' => $verbosity, '-class' => 'main' );
+my $dbg = Bio::DB::GenBank->new;
 my $fac = Bio::Phylo::Factory->new;
 my $project = $fac->create_project;
 my $taxa    = $fac->create_taxa;
@@ -32,20 +39,25 @@ for my $i ( @infile ) {
 	
 	# clean up row names: only using binomials
 	$matrix->visit(sub{
-		my $row  = shift;
-		my $name = $row->get_name;
-		$name =~ s/ .+//;
-		$name =~ s/_[^_]+$//;
-		$row->set_name($name);
-		$taxa{$name}++;
+		my $row = shift;
+		my $accession = $row->get_name;
+		$accession =~ s/ .+//; # strip trailing defline
+		$accession =~ s/^.+_([^_]+)$/$1/; # keep accession number
+		
+		# attempt to fetch taxon ID from sequence
+		$log->info("attempting to fetch sequence $accession");
+		my $seq = $dbg->get_Seq_by_acc($accession);
+		my $tid = $seq->species->ncbi_taxid;
+		$row->set_name($tid);
+		$taxa{$tid}++;
 	});
 	push @matrices, $matrix;
 }
 
 # populate the merged matrix
-for my $name ( sort { $a cmp $b } keys %taxa ) {
+NAME: for my $name ( sort { $a cmp $b } keys %taxa ) {
 
-	# assemble the concatenated sequence
+	# assemble the concatenated sequence, insert missing if need be
 	my $seq;
 	for my $m ( @matrices ) {
 		if ( my $row = $m->get_by_name($name) ) {
@@ -58,7 +70,7 @@ for my $name ( sort { $a cmp $b } keys %taxa ) {
 		}
 	}
 	
-	# create the taxon and datum objects
+	# create the taxon and datum objects, insert in containers
 	my $taxon = $fac->create_taxon( '-name' => $name );
 	$taxa->insert($taxon);	
 	$merged->insert($fac->create_datum( 
@@ -69,6 +81,7 @@ for my $name ( sort { $a cmp $b } keys %taxa ) {
 	));
 }
 
+# write output, can be nexus / phylip / nexml / fasta
 print unparse(
 	'-phylo'  => $project,
 	'-format' => $format,
