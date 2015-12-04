f658d901c36755666d41578457c0ff155e3e6f26
Rutger Vos <rutgeraldo@gmail.com>
2015-12-05 00:06:45 +0100

    almost working

--- script/relabel_tree.pl
+++ script/relabel_tree.pl
@@ -2,13 +2,14 @@
 use strict;
 use warnings;
 use Getopt::Long;
-use Bio::Phylo::IO 'parse_tree';
+use Bio::Phylo::IO qw'parse unparse';
 use Bio::Phylo::Util::Logger ':levels';
+use Bio::Phylo::Util::CONSTANT ':objecttypes';
 
 # process command line arguments
 my $verbosity = WARN;
 my $taxon_id  = 'taxon_id'; # column in spreadsheet
-my $original  = 'taxa_in_original_tree'; $ column in spreadsheet
+my $original  = 'taxa_in_original_tree'; # column in spreadsheet
 my $species   = 'species'; # column in spreadsheet
 my ( $intree, $data );
 GetOptions(
@@ -25,10 +26,12 @@ my $log = Bio::Phylo::Util::Logger->new(
 	'-level'  => $verbosity,
 	'-class'  => 'main',
 );
-my $tree = parse_tree(
+my $project = parse(
 	'-format' => 'figtree',
 	'-file'   => $intree,
+	'-as_project' => 1,
 );
+my ($tree) = @{ $project->get_items(_TREE_) };
 
 # read species table
 my %mapping;
@@ -49,31 +52,23 @@ my %mapping;
 			}
 			next LINE;
 		}
-		$mapping{ $line[$t_idx] } = [ $line[$c_idx], $line[$s_idx] ];	
+		$mapping{ $line[$t_idx] } = "'" . $line[$s_idx] . ' / ' . $line[$c_idx] . "'";	
 	}
 }
 
-# remap tree
-$tree->visit_dept_first(
-	'-post' => sub {
-		my $n = shift;
-		my @c = @{ $n->get_children };
-		
-		# node is a tip
-		if ( not @c ) {
-			my $name = $n->get_name;
-			if ( $mapping{$name} ) {
-				$n->set_name( $mapping{$name}->[1] );
-				$n->set_generic( 'clade' => $mapping{$name}->[0] );				
-			}
-			else {
-				$log->error("no mapping for $name");
-			}
-		}
-		
-		# node is internal, carry over the clade grouping
-		else {
-		
-		}	
+# relabel tips
+for my $tip ( @{ $tree->get_terminals } ) {
+	my $name = $tip->get_name;
+	if ( $mapping{$name} ) {
+		$tip->set_name($mapping{$name});
 	}
+	else {
+		$log->error("no mapping for $name");
+	}
+}
+
+print unparse(
+	'-format' => 'figtree',
+	'-phylo'  => $project,
+	'-nodelabels' => 1,
 );
