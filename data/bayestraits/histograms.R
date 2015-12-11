945ac1eb213bcc210ac1020ecb09411641ca67a3
rvosa <rutgeraldo@gmail.com>
2015-12-11 14:51:48 +0100

    now does K/S test, regenerated plot

--- data/bayestraits/histograms.R
+++ data/bayestraits/histograms.R
@@ -1,7 +1,17 @@
 
 table <- read.csv("outfile.txt", header = TRUE, sep = "\t")
-set.seed(42)
-p1 <- hist(table$q01,breaks=20)                     # centered at 4
-p2 <- hist(table$q10,breaks=20)                     # centered at 6
-plot( p1, col=rgb(0,0,1,1/4), xlim=c(0,15), ylim=c(0,200))  # first histogram
-plot( p2, col=rgb(1,0,0,1/4), xlim=c(0,15), ylim=c(0,200), add=T)  # second
+p1 <- hist(table$q01,breaks=30)
+p2 <- hist(table$q10,breaks=20)
+
+# plot the first histogram: rates for scalariform -> simple (purplish color)
+plot( p1, col=rgb(0,0,1,1/4), xlim=c(0,15), ylim=c(0,200), xlab="Instantaneous transition rate" )
+
+# add the second: rates for simple -> scalariform (salmon color)
+plot( p2, col=rgb(1,0,0,1/4), xlim=c(0,15), ylim=c(0,200), add=T)  
+
+# Two-sample Kolmogorov-Smirnov test
+# data:  table$q01 and table$q10
+# D^- = 0.923, p-value < 2.2e-16
+# alternative hypothesis: the CDF of x lies below that of y
+ks.test(table$q01,table$q10,alternative='l')
+
